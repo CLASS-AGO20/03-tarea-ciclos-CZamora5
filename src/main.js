00c5d523d167002d2f25b55469e19d915e4bc825
Carlos Zamora <czamora5@ucol.mx>
2020-12-10 14:30:21 -0600

    Included test cases for sumatoriaSerieUno method

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 export default class App {
 
     sumatoriaSerieUno(numero){
-        sum = 0;
-        for(i=1; i <= numero; i++){
+        let sum = 0;
+        for(let i=1; i <= numero; i++){
             sum += 1/i;
         }
         return sum;
@@ -10,3 +10,12 @@ export default class App {
 }
 
 let app = new App();
+
+//Probando método sumatoriaSerieUno(numero)
+let a = 23;
+let b = 4350;
+let c = 4576938;
+console.log(`Probando método sumatoriaSerieUno(numero):`);
+console.log(`La suma de los primeros ${a} términos de la serie es ${app.sumatoriaSerieUno(a)}`);
+console.log(`La suma de los primeros ${b} términos de la serie es ${app.sumatoriaSerieUno(b)}`);
+console.log(`La suma de los primeros ${c} términos de la serie es ${app.sumatoriaSerieUno(c)}`);
