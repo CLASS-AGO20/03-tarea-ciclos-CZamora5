dec928f850f8ede7b88a364642fd3c65a7401ec7
Carlos Zamora <czamora5@ucol.mx>
2020-12-10 14:20:23 -0600

    Implemented sumatoriaSerieUno method

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero){
+        sum = 0;
+        for(i=1; i <= numero; i++){
+            sum += 1/i;
+        }
+        return sum;
+    }
+}
 
 let app = new App();
